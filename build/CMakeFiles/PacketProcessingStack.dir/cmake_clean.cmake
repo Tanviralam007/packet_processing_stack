cfe30556d5b022e7019c1acfd457a3c2872050f6
Tanviralam007 <tanviralam451@gmail.com>
2025-02-11 18:48:08 +0600

    build added

---
+++ build/CMakeFiles/PacketProcessingStack.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PacketProcessingStack.dir/src/Packet.cpp.obj"
+  "CMakeFiles/PacketProcessingStack.dir/src/Packet.cpp.obj.d"
+  "CMakeFiles/PacketProcessingStack.dir/src/Process.cpp.obj"
+  "CMakeFiles/PacketProcessingStack.dir/src/Process.cpp.obj.d"
+  "CMakeFiles/PacketProcessingStack.dir/src/main.cpp.obj"
+  "CMakeFiles/PacketProcessingStack.dir/src/main.cpp.obj.d"
+  "PacketProcessingStack.exe"
+  "PacketProcessingStack.exe.manifest"
+  "PacketProcessingStack.pdb"
+  "libPacketProcessingStack.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PacketProcessingStack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/PacketProcessingStack.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for PacketProcessingStack.
+# This may be replaced when dependencies are built.
